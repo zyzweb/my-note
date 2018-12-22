6d1dedc855381cecc278c3dcaea85ac07c6de4a5
zyz1609062132 <1609062132@qq.com>
2018-12-22 11:48:53 +0800

    测试vux mid-list

--- src/components/slot-zyy-father.vue
+++ src/components/slot-zyy-father.vue
@@ -1,41 +1,44 @@
 <template>
-    <div class="father">
-        <h3>这是父组件</h3>
-        <child>
-            <!-- 第一次使用:用flex展示数据 -->
-            <template slot-scope="user">
-                <div class="tmp1">
-                    <span v-for="(item,index) in user.data" :key="index">{{item}}</span>
-                </div>
-            </template>
-            <!-- 第二次使用:用列表展示数据 -->
-            <!-- <template slot-scope="user">
+  <div class="father">
+    <h3>这是父组件</h3>
+    <child>
+      <!-- 第一次使用:用flex展示数据 -->
+      <template slot-scope="user">
+        <div class="tmp1">
+          <span
+            v-for="(item,index) in user.data"
+            :key="index"
+          >{{item}}</span>
+        </div>
+      </template>
+      <!-- 第二次使用:用列表展示数据 -->
+      <!-- <template slot-scope="user">
                <ul>
                    <li v-for="(item,index) in user.data" :key="index">{{item}}</li>
                </ul>
             </template> -->
-            <!-- 第三次使用:直接展示数据 -->
-            <!-- <template slot-scope="user">
+      <!-- 第三次使用:直接展示数据 -->
+      <!-- <template slot-scope="user">
                 {{user.data}}
             </template> -->
-        </child>
-    </div>
+    </child>
+  </div>
 </template>
 
 <script>
-import Child from './slot-zyy-son.vue'
+import Child from "./slot-zyy-son.vue";
 export default {
-    components: {
-        child: Child
-    }
-}
+  components: {
+    child: Child
+  }
+};
 </script>
 
 <style>
-    .tmp1 span{
-        width: 50px;
-        height: 50px;
-        border: 1px solid #000;
-    }
+.tmp1 span {
+  width: 50px;
+  height: 50px;
+  border: 1px solid #000;
+}
 </style>
 
