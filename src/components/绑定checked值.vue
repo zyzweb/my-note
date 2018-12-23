43c9e5bb230160647789d3e4a5d1b909744b2b95
zyz1609062132 <1609062132@qq.com>
2018-12-23 14:55:36 +0800

    绑定check demo

---
+++ src/components/绑定checked值.vue
@@ -0,0 +1,17 @@
+ <template>
+  <div>
+    <input type="checkbox" v-model="checked" id="checkbox">
+    <label for="checkbox">{{checked}}</label>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      checked:true
+    }
+  }
+}
+</script>
+ 
