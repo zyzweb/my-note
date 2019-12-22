a9018573e5de35115220dfb3f665c51b2c8a9d80
zyz1609062132 <zyz1609062132@gmail.com>
2019-12-22 23:34:18 +0800

    打印this.$slots

---
+++ src/components/son.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+    mounted() {
+    console.log('this.$slots', this.$slots);//{default:[VNode,_rendered: true]}
+    },
+}
+</script>
+
+<style>
+
+</style>
