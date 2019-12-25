508d7285a87f34d6a0bae3978f81c189bdb87d8c
zyz1609062132 <zyz1609062132@gmail.com>
2019-12-25 23:25:29 +0800

    异步请求方式

--- my-note/fetch axios.js
+++ my-note/fetch axios.js
@@ -20,6 +20,7 @@
 fetch('https://douban.uieee.com/v2/movie/top250').then(res=>res.json()).then(data=>console.log(data))
 
 //axios拦截响应(先写拦截器,再发请求)
+axios.interceptors.request.use(config => {console.log(config)})
 axios.interceptors.response.use(
     response => {
       if(response.data) {
@@ -27,4 +28,15 @@ axios.interceptors.response.use(
       }
     }
   )
-  axios.get("https://douban.uieee.com/v2/movie/top250").then(res => console.log(res.data))  //{age: 12}
+axios.get("https://douban.uieee.com/v2/movie/top250").then(res => console.log(res.data))  //{age: 12}
+
+
+//axios get post请求
+axios.post('http://127.0.0.1:8080/test/delUse',{key:value}).then(res => {})
+
+axios.get('http://127.0.0.1:8080/test/delUser?userId='+id).then(res => {})
+
+axios.get('http://127.0.0.1:8080/test/delUser',{ params:{ id:1 } }).then(res => {})
+
+axios({url:'http://127.0.0.1:8080/test/delUser', method:'post', data:{}})
+axios({url:'http://127.0.0.1:8080/test/delUser', method:'get', params:{key:value}})
