bd664859630a67002bcbc04b70846a69e40d34b6
zyz1609062132 <zyz1609062132@gmail.com>
2020-01-17 19:19:49 +0800

    测试1

--- src/components/test.vue
+++ src/components/test.vue
@@ -1,24 +1,23 @@
+
 <template>
   <div>
-    11
-    {{b}}
+    <icon-svg icon-class="dashboard" />
   </div>
 </template>
 
 <script>
+//引入svg组件
+import IconSvg from "./font-picture";
+
+//全局注册icon-svg
+// Vue.component("icon-svg", IconSvg);
+
 export default {
-  data() {
-    return {
-      a: {b:'ss'},
-      b:null
-    }
-  },
-  mounted() {
-    this.b = this.a.b
+  components: {
+    IconSvg
   }
-}
+};
 </script>
 
 <style>
-
 </style>
