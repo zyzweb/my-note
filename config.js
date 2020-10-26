15cd6ab9c7d2322b93af3a5eb97d46f0968e94dd
zyzweb <zyz1609062132@gmail.com>
2020-10-26 23:00:18 +0800

    添加config.js

---
+++ config.js
@@ -0,0 +1,45 @@
+// vue.code-snippets
+{
+	"Print to console": {
+	  "prefix": "vue",
+	  "body": [
+		"<template>",
+		"<div>",
+		  "$1",
+		"</div>",
+		"</template>",
+		"",
+		"<script>",
+		"  export default {",
+		"    data () {",
+		"      return {",
+					"$2",
+		"      }",
+		"    },",
+		"    created() {\n",
+		"    },",
+		"    mounted() {\n",
+		"    },",
+		"    methods: {\n",
+		"    }",
+		"  }",
+		"</script>",
+		"",
+		"<style lang='scss' scoped>",
+		"",
+		"</style>"
+	  ],
+	"description": "Log output to console"
+	}
+  }
+
+
+
+  "console.log": {
+    "prefix": "cl",
+    "body": [
+      "console.log('$1')",
+      "$2"
+    ],
+    "description": "console.log('$1')"
+  },
